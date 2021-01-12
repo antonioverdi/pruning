9a443202a55af1f3c3c531ddc2f2b5bfc4a700b4
antonioverdi <antonioverdicchio@gmail.com>
2021-01-12 11:46:51 -0800

    added test_models and gitignore

--- prune.py
+++ prune.py
@@ -3,8 +3,29 @@ import copy
 import torch
 import torch.nn as nn
 import torch.nn.utils.prune as prune
+import utils
 
-def prune_net(net):
+def greatest_delta(net):
+    """ Utility function for pruning network"""
+    if not isinstance(net, nn.Module):
+        print('Invalid input. Must be nn.Module')
+        return None
+    newnet = copy.copy(net)
+    modules_list = []
+
+    for name, module in newnet.named_modules():
+        if isinstance(module, torch.nn.Conv2d):
+            modules_list += [(module,'weight'),(module,'bias')]
+        if isinstance(module, torch.nn.Linear):
+            modules_list += [(module,'weight'),(module,'bias')]
+
+    prune.global_unstructured(
+        modules_list,
+        pruning_method=prune.L1Unstructured,
+        amount=0.2,)
+    return newnet
+
+def smallest_delta(net):
     """ Utility function for pruning network"""
     if not isinstance(net, nn.Module):
         print('Invalid input. Must be nn.Module')
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 """Utility functions for pruning experiments """
 import numpy as np
 import torch
+import math
 
 def create_dict(model):
     """Creates a dictionary containing all trainable parameters in a given model"""
@@ -13,29 +14,37 @@ def create_dict(model):
             module_dict[name] = module.weight.detach().numpy()
 
 def find_difference(first_array, second_array):
-    difference = np.subtract(second_array, first_array)
+    """ Returns an array that is equal to (second_array - first_array)"""
+    difference = np.absolute(np.subtract(second_array, first_array))
     return difference
 
-def find_smallest(array, n):
+def find_smallest(array, num):
     """ Finds the n smallest values in a given array"""
     orig_shape = array.shape
     array_flat = array.flatten()
     mask = np.ones_like(array_flat)
 
-    index_array = np.argpartition(array_flat, n)
+    if isinstance(num, float):
+        to_prune = math.ceil(num*len(array_flat))
+    elif isinstance(num, int):
+        to_prune = num
 
-    for i in range(n):
+    index_array = np.argpartition(array_flat, to_prune)
+
+    for i in range(to_prune):
         mask[index_array[i]] = 0
     mask.reshape(orig_shape)
+    return mask
 
-def find_largest(array, n):
+def find_largest(array, num):
     """ Finds the n smallest values in a given array"""
     orig_shape = array.shape
     array_flat = array.flatten()
     mask = np.ones_like(array_flat)
 
-    index_array = np.argpartition(array_flat, -n)
+    index_array = np.argpartition(array_flat, -num)
 
-    for i in range(n):
+    for i in range(num):
         mask[index_array[-i]] = 0
     mask.reshape(orig_shape)
+    return mask
